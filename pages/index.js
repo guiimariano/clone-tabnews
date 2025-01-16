262912ca20eac6552f726fbf344c912d9783d7b9
Guilherme Mariano <pro.gmariano@gmail.com>
2025-01-16 14:19:24 +0000

    Mensagem de motivação cumprindo o desafio proposto para enviar a amada!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Testando deploy</h1>
+    return (
+        <div>
+            <h1>Estou fazendo um curso novo!</h1>
+            <p>Estou criando uma página com uma tecnologia diferente e aprendendo conceitos que eu não sabia ou não lembrava.</p>
+            <p>Mas de antemão gostaria de dizer que <b>te amo</b> e vou lutar para melhorar nosso futuro.</p>
+        </div>
+    );
 }
 
 export default Home;
